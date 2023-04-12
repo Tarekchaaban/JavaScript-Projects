a504ee1db50ecdd17210da0a635c68eb99c79ecd
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-04-12 09:10:01 +0000

    halfway done with drumKit.

--- DrumKit/main.js
+++ DrumKit/main.js
@@ -1,3 +1,18 @@
 window.addEventListener('keydown', function (e) {
-  console.log(e.keyCode);
+  const audio = document.querySelector(
+    "audio[data-key='".concat(e.keyCode, "']")
+  );
+  const key = document.querySelector(".key[data-key='".concat(e.keyCode, "']"));
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.className = 'key playing';
 });
+const keys = document.querySelectorAll('.key');
+keys.forEach(function (key) {
+  return key.addEventListener('transitionend', removeTransition);
+});
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  console.log(e.propertyName);
+}
--- DrumKit/main.ts
+++ DrumKit/main.ts
@@ -1,3 +1,16 @@
 window.addEventListener('keydown', function (e) {
-  console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.className = 'key playing';
 });
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  console.log(e.propertyName);
+}
